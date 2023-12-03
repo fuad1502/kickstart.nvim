b6ce0364446c1e1e4194737f3751b861e95b1f9e
Fuad Ismail <fuad1502@gmail.com>
2023-12-03 12:30:55 +0700

    Refactored lsp keybinding settings in lspconfig on_attach function to custom/plugins/config/lsp-common.lua; Moved jdtls to seperate file and call lsp-common.set_keybinding on attach

--- init.lua
+++ init.lua
@@ -99,19 +99,7 @@ require('lazy').setup({
     },
   },
 
-  {
-    'mfussenegger/nvim-jdtls',
-    config = function()
-      local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-      local workspace_dir = '/home/fuad1502/.jdtls-workspace/' .. project_name
-      local config = {
-        cmd = { '/home/fuad1502/jdtls-1.9/bin/jdtls', '-data', workspace_dir },
-        root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-      }
-      require('jdtls').start_or_attach(config)
-    end,
-    ft = "java"
-  },
+  require 'custom.plugins.jdtls',
 
   {
     -- Autocompletion
@@ -470,46 +458,7 @@ end, 0)
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  require('custom.plugins.config.lsp-common').set_keybinding(bufnr)
 end
 
 -- document existing key chains
---
+++ lua/custom/plugins/jdtls.lua
@@ -0,0 +1,14 @@
+return {
+	'mfussenegger/nvim-jdtls',
+	config = function()
+		require('custom.plugins.config.lsp-common').set_keybinding(0);
+		local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+		local workspace_dir = '/home/fuad1502/.jdtls-workspace/' .. project_name
+		local config = {
+			cmd = { '/home/fuad1502/jdtls-1.9/bin/jdtls', '-data', workspace_dir },
+			root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+		}
+		require('jdtls').start_or_attach(config)
+	end,
+	ft = "java"
+}
