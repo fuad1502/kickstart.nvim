17308a5a0aa58bd2e5c2b08d2adc952ab05c24db
Fuad Ismail <fuad1502@gmail.com>
2024-01-14 18:31:55 +0700

    Added default mappings to nvim-tree for creating, deleting, and renaming files; Added open mapping that automatically closes the tree if it is a file

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -7,10 +7,25 @@ local function nvim_tree_on_attach(bufnr)
     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
 
+  local function open_file_and_close_tree()
+    -- get the current node
+    local node = api.tree.get_node_under_cursor()
+    if node then
+      api.node.open.edit()
+      -- if it's a file, close the tree
+      if node.type == 'file' then
+        api.tree.close()
+    end
+  end
+
   -- default mappings
-  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Filter: Git Ignore'))
+  vim.keymap.set('n', 'a', api.fs.create, opts('Create File Or Directory'))
+  vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
   -- custom mappings
   vim.keymap.set('n', '<leader>t', api.tree.toggle)
+  vim.keymap.set('n', '<CR>', open_file_and_close_tree, opts('Open'))
 end
 
 require("nvim-tree").setup {
